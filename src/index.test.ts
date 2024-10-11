d73f965cc10af4d81e5ef2b4d9922324da0e2244
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-10-11 10:58:56 +0200

    Add tests for reroll feature

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,24 @@
-import { describe, it, expect } from 'vitest';
+import { Game } from '$lib';
+import { describe, it, expect, beforeEach } from 'vitest';
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
+let game: Game = null!;
+
+beforeEach(() => {
+	game = new Game();
+	game.newRound();
+})
+
+describe('game test', () => {
+	it('should not be able to reroll more than 3 times', () => {
+		game.player.rerollWeapon();
+		game.player.rerollWeapon();
+		expect(() => game.player.rerollWeapon()).toThrowError('Cannot reroll weapon');
+	});
+
+	it('should have a different weapon after reroll', () => {
+		const weapon = game.player.weapon;
+		game.player.rerollWeapon();
+		expect(game.player.weapon).toBeDefined();
+		expect(game.player.weapon!.name).not.toBe(weapon!.name);
 	});
 });
