37bbe5ab5ea775122dba3e156d090ca506c913c1
Florian Nomel <florian.nomel@eemi.com>
2024-10-11 14:40:11 +0200

    Commit Florian Rendu

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,26 +1,22 @@
-
 import weapons from './weaponList.json';
 
 export let weaponList: any[] = [];
 
-export function init() {
+export interface GameStatus {
+    RoundInit: boolean;
+    RoundActive: boolean;
+    RoundPlayed: boolean;
+}
+
+let playerMaxHealth = 10;
+let playerCurrentHealth = 10;
+let enemyMaxHealth = 10;
+let enemyCurrentHealth = 10;
+
+export function init(): {playerMaxHealth : number, playerCurrentHealth : number, enemyCurrentHealth : number, enemyMaxHealth: number, playerWeapon : any, enemyWeapon : any, GameStatus : GameStatus} {
     weaponList = weapons;
-
-
-    let playerMaxHealth = 10;
-    let playerCurrentHealth = 10;
-    let enemyMaxHealth = 10;
-    let enemyCurrentHealth = 10;
     let playerWeapon = weaponList[Math.floor(Math.random() * weaponList.length)];
     let enemyWeapon = null;
-    let hasInit = true;
-    let hasRound = true;
-    let hasFought = false;
-    let playerWon = false;
-    let playerLost = false;
-
-    weaponList = weapons;
-
     return {
         playerMaxHealth,
         playerCurrentHealth,
@@ -28,136 +24,81 @@ export function init() {
         enemyCurrentHealth,
         playerWeapon,
         enemyWeapon,
-        hasInit,
-        hasRound,
-        hasFought,
-        playerWon,
-        playerLost
-    }
+        GameStatus:{
+            RoundInit: true,
+            RoundActive : true,
+            RoundPlayed : false,
+        }
+    };
 }
 
-export function newRound(hasInit: boolean) {
-    if(hasInit) {
-        weaponList = weapons;
-
-        return {
-            playerWeapon: weaponList[Math.floor(Math.random() * weaponList.length)],
-            enemyWeapon: null,
-            hasRound: true,
-            hasFought: false
-        }
-    } else {
-        throw new Error('Game not initialized');
+export function newRound(GameStatus: GameStatus): {playerWeapon: any, enemyWeapon: any, GameStatus: GameStatus} {
+    if(!GameStatus.RoundInit) {
+        throw new Error("Game not initialized");
     }
+    weaponList = weapons;
+    return {
+        playerWeapon: weaponList[Math.floor(Math.random() * weaponList.length)],
+        enemyWeapon: null,
+        GameStatus: {
+            ...GameStatus,
+            RoundActive: true,
+            RoundPlayed: false,
+        }
+    };
 }
 
-export function fight(playerHealth: number, enemyHealth: number, playerWeapon: any, hasInit: boolean, hasRound: boolean, hasFought: boolean): Array<number|boolean> {
-    
-    if(hasInit){
-        if(hasRound){
-            if(!hasFought) {
-    
-                let playerDamages: number = 0;
-                let enemyDamages: number = 0;
-            
-                switch (playerWeapon.name) {
-                    case 'hatchet':
-                    case 'knife':
-                    case 'spear':
-                        playerDamages += 1;
-                        break;
-                    case 'sword':
-                    case 'halberd': 
-                        playerDamages += 5;
-                        break;
-                    case 'bow':
-                        playerDamages += 1 * (Math.floor(Math.random() * 5));
-                        break;
-                    case 'crossbow':
-                        playerDamages += 2 * (Math.floor(Math.random() * 5));
-                        break
-                    case 'darts':
-                        playerDamages += 1 * (Math.floor(Math.random() * 3));
-                        break;
-                    case 'dagger':
-                        playerDamages += 3;
-                        break;
-                    default:
-                        throw new Error('Invalid weapon');
-                }
-            
-                // reset weapon list so the enemy could play
-                weaponList = weapons;
-            
-                let enemyWeapon = weaponList[Math.floor(Math.random() * weaponList.length)];
-            
-                switch (enemyWeapon.name) {
-                    case 'hatchet':
-                    case 'knife':
-                    case 'spear':
-                        enemyDamages += 1;
-                        break;
-                    case 'sword':
-                    case 'halberd': 
-                        enemyDamages += 5;
-                        break;
-                    case 'bow':
-                        enemyDamages += 1 * (Math.floor(Math.random() * 5));
-                        break;
-                    case 'crossbow':
-                        enemyDamages += 2 * (Math.floor(Math.random() * 5));
-                        break
-                    case 'darts':
-                        enemyDamages += 1 * (Math.floor(Math.random() * 3));
-                        break;
-                    case 'dagger':
-                        enemyDamages += 3;
-                        break;
-                    default:
-                        throw new Error('Invalid weapon');
-                }
+export function calculateDamage(weaponName: string): number {
+    const weaponDamageMap: { [key: string]: (randomFactor?: number) => number } = {
+        hatchet: () => 1,
+        knife: () => 1,
+        spear: () => 1,
+        sword: () => 5,
+        halberd: () => 5,
+        bow: () => 1 * Math.floor(Math.random() * 5),
+        crossbow: () => 2 * Math.floor(Math.random() * 5),
+        darts: () => 1 * Math.floor(Math.random() * 3),
+        dagger: () => 3,
+    };
+    const Damage = weaponDamageMap[weaponName];
+    if (Damage) {
+        return Damage();
+    }
+    throw new Error('Invalid weapon');
+}
 
-                if(playerDamages === enemyDamages) {
-                    return [playerHealth, enemyHealth];
-                }
-            
-                if(playerDamages > enemyDamages) {
-                    enemyHealth -= playerDamages - enemyDamages;
-                } else {
-                    playerHealth -= enemyDamages - playerDamages;
-                }
-           
-                // health cannot be negative
-                if(playerHealth <= 0) {
-                    playerHealth = 0;
-                }
-            
-                // health cannot be negative
-                if(enemyHealth <= 0) {
-                    enemyHealth = 0;
-                }
-                
-                // check if the game is over and the player has won
-                if(enemyHealth === 0) {
-                    return [playerHealth, enemyHealth, enemyWeapon, true, true, false];
-                }
+export function updateHealth(playerHealth: number, enemyHealth: number, playerDamages: number, enemyDamages: number): { updatedPlayerHealth: number, updatedEnemyHealth: number } {
+    enemyHealth = Math.max(0, enemyHealth - Math.max(0, playerDamages - enemyDamages));
+    playerHealth = Math.max(0, playerHealth - Math.max(0, enemyDamages - playerDamages));
 
+    return { updatedPlayerHealth: playerHealth, updatedEnemyHealth: enemyHealth };
+}
 
-                // check if the game is over and the player has lost
-                if(playerHealth === 0) {
-                    return [playerHealth, enemyHealth, enemyWeapon, true, false, true];
-                }
-            
-                return [playerHealth, enemyHealth, enemyWeapon, true, false, false];
-            }else{
-                throw new Error('Round already played');
-            }
-        }else{
-            throw new Error('Round not initialized');
-        }
-    }else{
+function GetEnemyWeapon() {
+    return weaponList[Math.floor(Math.random() * weaponList.length)];
+}
+
+export function fight(playerHealth: number, enemyHealth: number, playerWeapon: any, GameStatus: GameStatus): Array<number | boolean> {
+    if (!GameStatus.RoundInit) {
         throw new Error('Game not initialized');
     }
-
-
+    if (!GameStatus.RoundActive) {
+        throw new Error('Round not initialized');
+    }
+    if (GameStatus.RoundPlayed) {
+        throw new Error('Round already played');
+    }            
+    const playerDamages = calculateDamage(playerWeapon.name);
+    const enemyWeapon = GetEnemyWeapon();
+    const enemyDamages = calculateDamage(enemyWeapon.name);
+    const { updatedPlayerHealth, updatedEnemyHealth } = updateHealth(playerHealth, enemyHealth, playerDamages, enemyDamages);
+    // Vérification des conditions de victoire ou de défaite
+    if (updatedEnemyHealth === 0) {
+        return [updatedPlayerHealth, updatedEnemyHealth, enemyWeapon, true, true, false];
+    }
+    if (updatedPlayerHealth === 0) {
+        return [updatedPlayerHealth, updatedEnemyHealth, enemyWeapon, true, false, true];
+    }
+    GameStatus.RoundPlayed = true;
+    return [updatedPlayerHealth, updatedEnemyHealth, enemyWeapon, true, false, false];
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,112 +1,114 @@
 <script lang="ts">
-
     import { fight, init, newRound } from "$lib";
-
+    import type { GameStatus } from "$lib";
+    
     let state: any = {
+        RoundInit: false,
+        RoundActive: false,
+        RoundPlayed: false,
         playerMaxHealth: null,
         playerCurrentHealth: null,
         enemyMaxHealth: null,
         enemyCurrentHealth: null,
         playerWeapon: null,
         enemyWeapon: null,
-        hasInit: false,
-        hasRound: false,
-        hasFought: false,
-        playerWon: false,
-        playerLost: false
     };
-
+    
     function triggerInit() {
         state = init();
     }
-
+    
     function triggerNewRound() {
         let response = null;
         try {        
-            response = newRound(state.hasInit);
+            response = newRound(state);
         } catch (error) {
             console.error(error);
         }
-
-        if(response !== null) {
+    
+        if (response !== null) {
             state.playerWeapon = response.playerWeapon;
             state.enemyWeapon = response.enemyWeapon;
-            state.hasRound = response.hasRound;
-            state.hasFought = response.hasFought;
+            state.RoundActive = true;  // Mise à jour de l'état du round
+            state.RoundPlayed = false; // Remet à faux pour le nouveau round
         }
-
     }
-
+    
     function triggerFight() {
         let response = null;
-
+    
         try {        
-            response = fight(state.playerCurrentHealth, state.enemyCurrentHealth, state.playerWeapon, state.hasInit, state.hasRound, state.hasFought);
+            response = fight(state.playerCurrentHealth, state.enemyCurrentHealth, state.playerWeapon, state);
         } catch (error) {
             console.error(error);
         }
-
-        if(response !== null) {
+    
+        if (response !== null) {
             state.playerCurrentHealth = response[0];
             state.enemyCurrentHealth = response[1];
             state.enemyWeapon = response[2];
-            state.hasFought = response[3];
-            state.playerWon = response[4];
-            state.playerLost = response[5];
+            state.RoundPlayed = response[3];
+            state.roundActive = true; // Le round est toujours actif après le combat
+            if (response[4]) {
+                // Si le joueur a gagné
+                state.roundPlayed = true; // Le round est joué
+            }
+            if (response[5]) {
+                // Si le joueur a perdu
+                state.RoundPlayed = true; // Le round est joué
+            }
         }
     }
-
-</script>
-
-
-<section id="player" class="w-1/3">
-    {#if state.hasInit === true}
-        <div class="flex flex-row items-center justify-between flex-wrap w-full">
-            <div class="flex flex-col items-center justify-center w-full">
-                <h1 class="text-2xl font-bold">Player</h1>
-                <p class="text-lg">Health: {state.playerCurrentHealth} / {state.playerMaxHealth}</p>
-                <p class="text-lg">Weapon name: {state.playerWeapon.name}</p>
-                <p class="text-lg">Weapon description: {state.playerWeapon.description}</p>
+    </script>
+    
+    <section id="player" class="w-1/3">
+        {#if state.RoundInit}
+            <div class="flex flex-row items-center justify-between flex-wrap w-full">
+                <div class="flex flex-col items-center justify-center w-full">
+                    <h1 class="text-2xl font-bold">Player</h1>
+                    <p class="text-lg">Health: {state.playerCurrentHealth} / {state.playerMaxHealth}</p>
+                    <p class="text-lg">Weapon name: {state.playerWeapon?.name}</p>
+                    <p class="text-lg">Weapon description: {state.playerWeapon?.description}</p>
+                </div>
             </div>
-        </div>
-    {/if}
-</section>
-
-<section id="action">
-    {#if state.hasInit === false}
-        <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Start</button>
-    {:else}
-        {#if (state.hasRound === true && state.hasFought === true && state.playerWon === false && state.playerLost === false)}
-            <button class="btn btn-xl variant-filled-warning" on:click={triggerNewRound}>Next Round</button>
         {/if}
-
-        {#if (state.hasRound === true && state.hasFought === false && state.playerWon === false && state.playerLost === false)}
-            <button class="btn btn-xl variant-filled-error" on:click={triggerFight}>Fight</button>
+    </section>
+    
+    <section id="action">
+        {#if !state.RoundInit}
+            <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Start</button>
+        {:else}
+            {#if state.RoundActive && !state.RoundPlayed}
+                <button class="btn btn-xl variant-filled-error" on:click={triggerFight}>Fight</button>
+            {/if}
+    
+            {#if state.RoundActive && state.RoundPlayed && !state.playerWon && !state.playerLost}
+                <button class="btn btn-xl variant-filled-warning" on:click={triggerNewRound}>Next Round</button>
+            {/if}
+    
+            {#if state.RoundActive && state.RoundPlayed && state.playerWon}
+                <p class="p">You won!</p>
+                <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Play again</button>
+            {/if}
+    
+            {#if state.RoundActive && state.RoundPlayed && state.playerLost}
+                <p class="p">You lost...</p>
+                <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Play again</button>
+            {/if}
         {/if}
-
-        {#if (state.hasRound === true && state.hasFought === true && state.playerWon === true && state.playerLost === false)}
-            <p class="p">You won !</p>
-            <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Play again</button>
-        {/if}
-
-        {#if (state.hasRound === true && state.hasFought === true && state.playerWon === false && state.playerLost === true)}
-            <p class="p">You lost ...</p>
-            <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Play again</button>
-        {/if}
-    {/if}
-</section>
-
-<section id="enemy" class="w-1/3">
-    {#if state.hasInit === true}
-        <div class="flex flex-row items-center justify-between flex-wrap w-full">
-            <div class="flex flex-col items-center justify-center w-full">
-                <h1 class="text-2xl font-bold">Enemy</h1>
-                <p class="text-lg">Health: {state.enemyCurrentHealth} / {state.enemyMaxHealth}</p>
-                {#if state.enemyWeapon !== null}
-                    <p class="text-lg">Weapon name: {state.enemyWeapon.name}</p>
-                    <p class="text-lg">Weapon description: {state.enemyWeapon.description}</p>
-                {/if}
+    </section>
+    
+    <section id="enemy" class="w-1/3">
+        {#if state.RoundInit}
+            <div class="flex flex-row items-center justify-between flex-wrap w-full">
+                <div class="flex flex-col items-center justify-center w-full">
+                    <h1 class="text-2xl font-bold">Enemy</h1>
+                    <p class="text-lg">Health: {state.enemyCurrentHealth} / {state.enemyMaxHealth}</p>
+                    {#if state.enemyWeapon}
+                        <p class="text-lg">Weapon name: {state.enemyWeapon.name}</p>
+                        <p class="text-lg">Weapon description: {state.enemyWeapon.description}</p>
+                    {/if}
+                </div>
             </div>
-        </div>
-    {/if}
-</section>
+        {/if}
+    </section>    
