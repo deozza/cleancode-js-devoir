d6ba7b6e82aad1e08b6f47aa1c086443b448b6c0
marilyne.leam <marilyne.leam@eemi.com>
2024-10-11 12:21:29 +0200

    add class

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,49 +25,49 @@
     }
 </script>
 
-<section id="player" class="w-1/3">
-    {#if !gameState.isGameOver}
-        <div class="flex flex-row items-center justify-between flex-wrap w-full">
-            <div class="flex flex-col items-center justify-center w-full">
-                <h1 class="text-2xl font-bold">Player</h1>
-                <p class="text-lg">Health: {gameState.player.currentHealth} / {gameState.player.maxHealth}</p>
-                <p class="text-lg">Weapon name: {gameState.player.weapon.name}</p>
-                <p class="text-lg">Weapon description: {gameState.player.weapon.description}</p>
-            </div>
-        </div>
-    {/if}
-</section>
-
-<section id="action">
+<div class="flex justify-center items-center min-h-screen w-full text-white">
     {#if gameState.isGameOver}
-        {#if gameState.winner === 'player'}
-            <p class="p">You won!</p>
-        {:else if gameState.winner === 'enemy'}
-            <p class="p">You lost...</p>
-        {/if}
-        <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Play again</button>
+        <div class="text-center">
+            {#if gameState.winner === 'player'}
+                <p class="text-2xl mb-4">You won!</p>
+            {:else if gameState.winner === 'enemy'}
+                <p class="text-2xl mb-4">You lost...</p>
+            {/if}
+            <button class="btn btn-xl variant-filled-primary" on:click={triggerInit}>Play again</button>
+        </div>
     {:else}
-        <button class="btn btn-xl variant-filled-error" on:click={triggerFight}>Fight</button>
-        {#if gameState.round > 1}
-            <button class="btn btn-xl variant-filled-warning" on:click={triggerNewRound}>Next Round</button>
-        {/if}
-        {#if gameState.playerWeaponChanges < 2}
-            <button class="btn btn-xl variant-filled-secondary" on:click={triggerChangeWeapon}>
-                Change Weapon ({2 - gameState.playerWeaponChanges} left)
-            </button>
-        {/if}
-    {/if}
-</section>
+        <div class="flex justify-center items-center w-full">
+            <section id="player" class="w-1/3 text-center">
+                <div class="flex flex-col items-center justify-center w-full">
+                    <h1 class="text-2xl font-bold">Player</h1>
+                    <p class="text-lg">Health: {gameState.player.currentHealth} / {gameState.player.maxHealth}</p>
+                    <p class="text-lg">Weapon name: {gameState.player.weapon.name}</p>
+                    <p class="text-lg">Weapon description: {gameState.player.weapon.description}</p>
+                </div>
+            </section>
 
-<section id="enemy" class="w-1/3">
-    {#if !gameState.isGameOver}
-        <div class="flex flex-row items-center justify-between flex-wrap w-full">
-            <div class="flex flex-col items-center justify-center w-full">
-                <h1 class="text-2xl font-bold">Enemy</h1>
-                <p class="text-lg">Health: {gameState.enemy.currentHealth} / {gameState.enemy.maxHealth}</p>
-                <p class="text-lg">Weapon name: {gameState.enemy.weapon.name}</p>
-                <p class="text-lg">Weapon description: {gameState.enemy.weapon.description}</p>
-            </div>
+            <section id="action" class="flex flex-col items-center justify-center">
+                <div class="flex flex-col items-center space-y-4">
+                    <button class="btn btn-xl variant-filled-error" on:click={triggerFight}>Fight</button>
+                    {#if gameState.round > 1}
+                        <button class="btn btn-xl variant-filled-warning" on:click={triggerNewRound}>Next Round</button>
+                    {/if}
+                    {#if gameState.playerWeaponChanges < 2}
+                        <button class="btn btn-xl variant-filled-secondary" on:click={triggerChangeWeapon}>
+                            Change Weapon ({2 - gameState.playerWeaponChanges} left)
+                        </button>
+                    {/if}
+                </div>
+            </section>
+
+            <section id="enemy" class="w-1/3 text-center">
+                <div class="flex flex-col items-center justify-center w-full">
+                    <h1 class="text-2xl font-bold">Enemy</h1>
+                    <p class="text-lg">Health: {gameState.enemy.currentHealth} / {gameState.enemy.maxHealth}</p>
+                    <p class="text-lg">Weapon name: {gameState.enemy.weapon.name}</p>
+                    <p class="text-lg">Weapon description: {gameState.enemy.weapon.description}</p>
+                </div>
+            </section>
         </div>
     {/if}
-</section>
+</div>
